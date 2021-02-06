84a4964b63d12bb305eccd86f38a114bff210c06
Luca Martinelli <lucxmangajet@gmail.com>
2021-02-06 14:36:29 +0100

    added buttons to change size and resolution

--- presentations_mod/init.lua
+++ presentations_mod/init.lua
@@ -33,7 +33,9 @@ local DisplayEntity = {
     },
 
     id = -1,
-    texture_name = "img.png"
+    texture_name = "img.png",
+    proportions = 1.0,
+    size = 1.0
 }
 
 function DisplayEntity:change_texture_to(texture)
@@ -41,12 +43,38 @@ function DisplayEntity:change_texture_to(texture)
     self.object:set_properties ({textures = {texture}})
 end
 
+function DisplayEntity:set_proportions(new_proportions)
+    self.proportions = new_proportions
+    self:adapt_size()
+end
+
+function DisplayEntity:set_size(new_size)
+    self.size = new_size
+    self:adapt_size()
+end
+
+function  DisplayEntity:adapt_size()
+    
+    local size_x = self.size;
+    local size_y = self.size * self.proportions;
+    local half_x = size_x * 0.5
+    local half_y = size_y * 0.5
+
+    self.object:set_properties({
+        visual_size = {x = size_x, y = size_y},
+        collisionbox = {-half_x, -half_y, -.1, half_x, half_y, .1}
+    })
+end
+
 function DisplayEntity:on_activate(staticdata, dtime_s)
     
     if staticdata ~= nil and staticdata ~= "" then
         local data = minetest.parse_json(staticdata)
         self.id = data.id
+        self.proportions = data.proportions
+        self.size = data.size
         self:change_texture_to(data.texture_name)
+        self:adapt_size()
     end
     
     if self.id <0 then
@@ -62,14 +90,14 @@ end
 function  DisplayEntity:get_staticdata()
     return minetest.write_json({
         id = self.id,
-        texture_name = self.texture_name
+        texture_name = self.texture_name,
+        proportions = self.proportions,
+        size = self.size
     })
 end
 
 
 function DisplayEntity:on_rightclick(clicker)
-    
-
     self:show_formspec(clicker)
 end
 
@@ -83,19 +111,49 @@ minetest.register_on_player_receive_fields(function(player, formname, fields)
     end
 
     local id = tonumber(string.sub(formname, display_formspec_name:len()+1));
-    msg_player(player, "Display formspec recieved: " .. id)
+    local display = displays[id]
+    if not display then
+        msg_player(player, "Error: no display found with id " .. id)
+        return
+    end
 
-    if fields.URL then
+    if fields.URL and fields.URL ~= "" then
 
         if ends_with(fields.URL, ".png") or ends_with(fields.URL, ".jpg") then
             local resName = downloadAndSaveTexture(player, fields.URL)
             if resName then
                 msg_player(player, "Saved " .. resName)
-                displays[id]:change_texture_to(resName)
+                display:change_texture_to(resName)
             end
         else
             msg_player(player, "Only .png and .jpg are supported. Invalid URL: " .. fields.URL )
         end
+    end
+
+    if fields.UP then
+        msg_player(player, "UP pressed")
+        display:set_size(display.size+1)
+    end
+
+    if fields.DOWN then
+        msg_player(player, "DOWN pressed")
+        display:set_size(display.size-1)
+    end
+
+    if fields.R16_9 then
+        display:set_proportions(0.5625)
+    end
+
+    if fields.R4_3 then
+        display:set_proportions(0.75)
+    end
+
+    if fields.R5_4 then
+        display:set_proportions(0.8)
+    end
+
+    if fields.R1_1 then
+        display:set_proportions(1.0)
     end
 end)
 
@@ -114,9 +172,15 @@ end)
 
 local string testSpec = 
 "formspec_version[4]" ..
-"size[10,5]" ..
+"size[10,10]" ..
 "label[1,1;Please insert URL Here:]" ..
-"field[1,2;8,1;URL;URL;]"
+"field[1,2;8,1;URL;URL;]" ..
+"button[1,4; 1,1;UP;UP;]" ..
+"button[2,4; 1,1;DOWN;DOWN;]"  ..
+"button[1,5; 1,1;R16_9;16:9;]"  ..
+"button[2,5; 1,1;R4_3;4:3;]"  ..
+"button[3,5; 1,1;R5_4;5:4;]"  ..
+"button[4,5; 1,1;R1_1;1:1;]"  
 
 minetest.register_entity(Modname .. ':display', DisplayEntity)
 
@@ -128,31 +192,16 @@ function downloadAndSaveTexture(requester ,url)
 		local client, code, headers, status = Http.request({url=url, sink=Ltn12.sink.table(resp), method=method })
             
         if code == 200 then
-			
             if resp then
                 local data = table.concat(resp);
                 if data then
-                    --minetest.chat_send_all("DataLength: " .. data);
-                    --minetest.chat_send_all(hex_dump(data))
                     local name = url:match( "([^/]+)$" )
-
                     local path =  Modpath .. DIR_DELIM .. "textures" .. DIR_DELIM .. name
                     local file = io.open(path, "w+")
                     io.output(file)
                     io.write(data)
                     io.close(file)
-
-                    --for key,value in pairs(minetest) do
-                      --  if(key)
-                      --  minetest.chat_send_all(key);
-                    --end
-
                     minetest.dynamic_add_media(path)
-                    --    minetest.chat_send_all( name .. "confirmed")
-                    --end)
-
-                    --minetest.chat_send_all("Dynamic addition: " .. res)
-                    --minetest.chat_send_all("Saving " .. name .. " to: " .. path)
                     return name
                 end
                 return nil
@@ -161,31 +210,4 @@ function downloadAndSaveTexture(requester ,url)
             msg_player(requester, "ERROR: " ..code)
             return nil
         end
-end
-
-function hex_dump (str)
-    local len = string.len( str )
-    local dump = ""
-    local hex = ""
-    local asc = ""
-    
-    for i = 1, len do
-        if 1 == i % 8 then
-            dump = dump .. hex .. asc .. "\n"
-            hex = string.format( "%04x: ", i - 1 )
-            asc = ""
-        end
-        
-        local ord = string.byte( str, i )
-        hex = hex .. string.format( "%02x ", ord )
-        if ord >= 32 and ord <= 126 then
-            asc = asc .. string.char( ord )
-        else
-            asc = asc .. "."
-        end
-    end
-
-    
-    return dump .. hex
-            .. string.rep( "   ", 8 - len % 8 ) .. asc
 end
